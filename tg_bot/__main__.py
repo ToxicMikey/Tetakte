b0a19d96698648bb135a481f4eca18576bb75d75
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-07-25 16:25:56 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -132,12 +132,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
-                PM_START_TEXT.format(escape_markdown(bot.first_name)), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="➕ Add Me to Your Group ➕", 
-                                                                                                                                                 url="t.me/{}?startgroup=true".format(bot.username)), 
-                                                                                                                                                  InlineKeyboardButton(text="🎲 Command",
-                                                                                                                                                 url="https://t.me/{}?start=help".format(bot.username)),
-                                                                                                                                                  InlineKeyboardButton(text="🎁 Source Code",
-                                                                                                                                                 url="https://github.com/RSR-TG-Info/Tetakte")]])
+                PM_START_TEXT.format(escape_markdown(bot.first_name)), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="➕ Add Me to Your Group ➕", url="t.me/{}?startgroup=true".format(bot.username)), InlineKeyboardButton(text="🎲 Command", url="https://t.me/{}?start=help".format(bot.username))]])
                 
                 
     else:
